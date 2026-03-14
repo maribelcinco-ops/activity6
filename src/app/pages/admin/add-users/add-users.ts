a8077bb9d0fc9e6b73e2209b21cd4b9504c65e69
Maribel S. Cinco <maribel.cinco@evsu.edu.ph>
2026-03-14 10:58:30 +0800

    Activity 7

--- src/app/pages/admin/add-users/add-users.ts
+++ src/app/pages/admin/add-users/add-users.ts
@@ -7,43 +7,43 @@ import { UsersModel } from '../../../models/UsersModel';
 @Component({
   selector: 'app-add-users',
   standalone: true,
-  imports:[CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-users.html',
   styleUrls: ['./add-users.css']
 })
+export class AddUsers {
 
-export class AddUsers{
+  registerForm: FormGroup;
+  users: UsersModel[] = [];
+  message = "";
 
-  registerForm:FormGroup;
-  users:UsersModel[]=[];
-  message="";
+  constructor(private fb: FormBuilder, private userService: UsersService) {
 
-  constructor(private fb:FormBuilder, private userService:UsersService){
-
-    this.registerForm=this.fb.group({
-      email:['',[Validators.required,Validators.email]],
-      username:['',Validators.required],
-      password:['',Validators.required],
-      role:['Student']
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['Student', Validators.required]
     });
 
   }
 
-  register(){
+  register() {
 
-    if(this.registerForm.valid){
-
-      const user:UsersModel=this.registerForm.value;
-
-      this.userService.addUser(user);
-
-      this.users=this.userService.getUsers();
-
-      this.message="Registration successful";
-
-      this.registerForm.reset({role:'Student'});
-
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
     }
+
+    const user: UsersModel = this.registerForm.value;
+
+    this.userService.addUser(user);
+
+    this.users = this.userService.getUsers();
+
+    this.message = "Registration successful";
+
+    this.registerForm.reset({ role: 'Student' });
 
   }
 
